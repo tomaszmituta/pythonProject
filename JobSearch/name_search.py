a00e05a6b316cfae1eb4c25040501c35780d3dfb
tomas <tomasz.mitta@gmail.com>
2020-09-02 16:05:48 +0200

    Merge remote-tracking branch 'origin/master' into master

--- JobSearch/name_search.py
+++ JobSearch/name_search.py
@@ -13,10 +13,10 @@ sleep(3)
 
 username = webdriver.find_element_by_name('session_key')
 #enter yor e-mail
-username.send_keys('ID')
+username.send_keys('tomasz.mituta@gmail.com')
 password = webdriver.find_element_by_name('session_password')
 #enter pass to linkied in
-password.send_keys('pass')
+password.send_keys('Tomasz01+')
 
 #enter to page
 login_in_enter = webdriver.find_element_by_name("session_password").send_keys(Keys.RETURN)
@@ -26,23 +26,34 @@ search=webdriver.find_element_by_xpath(
                 '/html/body/header/div/form/div/div/div/div/div/div/input')
 
 sleep(5)
-search.send_keys("#jezierski")
+search.send_keys("#skills #sap #sd")
 sleep(5)
 search.send_keys(Keys.RETURN)
 sleep(5)
-click_name=webdriver.find_element_by_class_name('name-and-distance')
-click_name.click()
-sleep(5)
-#send Message
-send_message=webdriver.find_element_by_xpath(
-                '/html/body/div[8]/div[3]/div/div/div/div/div[2]/main/div/section/div[2]/div/div[2]/div/div/span[2]/div/div')
-sleep(5)
-send_message.click()
-sleep(5)
+
+people_click=webdriver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div/header/div/div/div/ul/li/button/span') #sometimes 7 to 8- only this is not working.not sure why ? look if this is not wokrkin try this.
+sleep(4)
+people_click.click()
+sleep(4)
+# click_name=webdriver.find_element_by_class_name('name-and-distance')
+# sleep(4)
+# click_name.click()
+
+# sleep(5)
+# #send Message
+# send_message=webdriver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div/div/div/div[2]/main/div/section/div[2]/div/div[2]/div/div/span[2]/div/div')
+# sleep(5)
+# send_message.click()
+# sleep(5)
 # #back
 # back_to_search=webdriver.back()
 # sleep(5)
+# #next Name
+# find_next_name= webdriver.find_element_by_class_name('name-and-distance') ++1
+# click_name.click()
 
-#hire will be loop for click_name +1 as this needs run ++1
+
+
+
 
 
--- JobSearch/sandbox.py
+++ JobSearch/sandbox.py
@@ -4,7 +4,7 @@ from time import sleep, strftime
 from random import randint
 import pandas as pd
 
-chromedriver_path = 'C:/Users/tomas/PycharmProjects/All-Projects/Instagram/chromedriver.exe'  # Change this to your own chromedriver path!
+chromedriver_path = 'C:/Users/tomas/PycharmProjects/pythonProject/Instagram/chromedriver.exe'  # Change this to your own chromedriver path!
 webdriver = webdriver.Chrome(executable_path=chromedriver_path)
 sleep(10)
 webdriver.get('https://www.linkedin.com/')
@@ -13,10 +13,10 @@ sleep(3)
 
 username = webdriver.find_element_by_name('session_key')
 #enter yor e-mail
-username.send_keys('ID')
+username.send_keys('tomasz.mituta@gmail.com')
 password = webdriver.find_element_by_name('session_password')
 #enter pass to linkied in
-password.send_keys('PASS')
+password.send_keys('Tomasz01+')
 
 #enter to page
 login_in_enter = webdriver.find_element_by_name("session_password").send_keys(Keys.RETURN)
@@ -26,30 +26,31 @@ search=webdriver.find_element_by_xpath(
                 '/html/body/header/div/form/div/div/div/div/div/div/input')
 
 sleep(5)
-search.send_keys("#Jezierski")
+search.send_keys("#skills #sap #sd #job")
 sleep(5)
 search.send_keys(Keys.RETURN)
 sleep(5)
-name = webdriver.__class__('name actor-name')
-name_number =0
-name_table = webdriver.find_element_by_class_name('name-and-icon')
-print(name)
-
-# musimy policzyc ile jest w name actor-name na stronie , pozniej next page.
-
-
-#
-# click_name=webdriver.find_element_by_class_name('name-and-distance')
-# click_name.click()
-# sleep(5)
-# #send Message
-# send_message=webdriver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div/div/div/div[2]/main/div/section/div[2]/div/div[2]/div/div/span[2]/div/div')
-# sleep(5)
-# send_message.click()
-# sleep(5)
-# #back
-# back_to_search=webdriver.back()
+#job search
+job_click=webdriver.find_element_by_xpath(
+                '/html/body/div[8]/div[3]/div/div/header/div/div/div/ul/li[2]/button/span')
 sleep(5)
+job_click.click()
+sleep(4)
+#job search by the location
+job_search_by_location =webdriver.find_element_by_xpath(
+                '/html/body/header/div/form/div/div/div/div/div[3]/div/div/input')
+sleep(5)
+job_search_by_location.clear()
+sleep(1)
+job_search_by_location.send_keys("usa")
+sleep(4)
+#job_search_by_location.send_keys(Keys.RETURN)
+Search_click =webdriver.find_element_by_xpath(
+                 '/html/body/header/div/form/div/div/div/div/button')
+sleep(3)
+Search_click.click()
+
+
 
 
 
